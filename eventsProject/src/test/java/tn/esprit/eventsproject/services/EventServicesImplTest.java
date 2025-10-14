cc9e5d639210941db9f3f6377b5f1bd6996385d2
medkhalilbensmida <khalilsd2002@gmail.com>
2025-10-15 00:13:56 +0100

    ✅ Fix Mockito imports and dependencies

--- eventsProject/src/test/java/tn/esprit/eventsproject/services/EventServicesImplTest.java
+++ eventsProject/src/test/java/tn/esprit/eventsproject/services/EventServicesImplTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.eventsproject.entities.Event;
 import tn.esprit.eventsproject.entities.Participant;
@@ -18,6 +19,7 @@ import static org.junit.Assert.*;
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class EventServicesImplTest {
 
     @Autowired
