d4fd87b84669b3c755ec9c0461142f926fd6766f
oussema <oussemahamouda6@gmail.com>
2025-11-16 13:12:50 +0100

    iam role

--- eventsProject/infra/main.tf
+++ eventsProject/infra/main.tf
@@ -1,21 +1,52 @@
+# -------------------------------
+#         DATA SOURCES
+# -------------------------------
+data "aws_availability_zones" "available" {}
+
+# -------------------------------
+#          VPC MODULE
+# -------------------------------
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+# -------------------------------
+#          EKS MODULE
+# -------------------------------
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.8.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  # OLD STYLE ARGUMENTS FOR MODULE v21.x
+  name    = var.cluster_name       # cluster name
+  version = "1.29"                 # Kubernetes version
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  manage_aws_auth = true
-
-  eks_managed_node_groups = {
+  # Node groups (managed nodes)
+  node_groups = {
     default = {
-      min_size       = 1
-      desired_size   = var.desired_size
-      max_size       = 3
-      instance_types = [var.node_instance_type]
+      desired_capacity = var.desired_size
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type    = var.node_instance_type
     }
+  }
+
+  tags = {
+    Environment = "jenkins-eks"
+    ManagedBy   = "terraform"
   }
 }
