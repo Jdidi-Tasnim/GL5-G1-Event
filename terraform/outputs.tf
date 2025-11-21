aa1a881dd0a92b0d2e7da819dbc4d8ec41b1d604
AchrefSaoud <achraf.saoud@etudiant-fst.utm.tn>
2025-11-21 15:54:27 +0100

    Add AWS EKS deployment with Terraform and Kubernetes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,28 +1,11 @@
-# EKS Cluster Outputs
-output "cluster_id" {
-  description = "EKS cluster ID"
-  value       = module.eks.cluster_id
-}
-
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
-}
-
 output "cluster_name" {
-  description = "Kubernetes Cluster Name"
+  description = "EKS cluster name"
   value       = module.eks.cluster_name
 }
 
-output "cluster_certificate_authority_data" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = module.eks.cluster_certificate_authority_data
-  sensitive   = true
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ID attached to the EKS cluster"
-  value       = module.eks.cluster_security_group_id
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
 }
 
 output "region" {
@@ -30,12 +13,7 @@ output "region" {
   value       = var.aws_region
 }
 
-output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
-}
-
 output "configure_kubectl" {
-  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  description = "Configure kubectl command"
   value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
 }
